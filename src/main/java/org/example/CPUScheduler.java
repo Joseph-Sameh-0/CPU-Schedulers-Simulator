f3cfa0a68aa2a9b3e1cb90961500f53f7cb933bc
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2024-12-08 20:51:13 +0200

    .

--- src/main/java/org/example/CPUScheduler.java
+++ src/main/java/org/example/CPUScheduler.java
@@ -2,38 +2,13 @@ package org.example;
 
 import org.example.FCAI.*;
 
-
 public class CPUScheduler {
 
   static FCAISchedular schedular;
 
   public static void main(String[] args) {
-    new Thread(() -> {
-      int g = 40;
-      while (g-- > 0) {
-        try {
-          Thread.sleep(500);
-          System.out.println(
-            "------------------------------------------------------------------------- time " + (39 - g) + " -> " +(40 - g)
-          );
-          Thread.sleep(500);
-        } catch (InterruptedException e) {
-          Thread.currentThread().interrupt();
-          break;
-        }
-      }
-    })
-      .start();
-
     schedular = new FCAISchedular();
-
-    //get processes
     schedular.setProcesses("src/test_cases/AG.txt");
-    // List<Process> processes = getProcesses();
-
-    // schedular.setProcesses(processes);
-    //start
-
     schedular.run();
   }
 }
--- src/main/java/org/example/FCAI/FCAISchedular.java
+++ src/main/java/org/example/FCAI/FCAISchedular.java
@@ -4,7 +4,301 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+class CustomQueue<T extends Comparable<T>> {
+
+  private final List<T> list;
+
+  public CustomQueue() {
+    this.list = Collections.synchronizedList(new ArrayList<>());
+  }
+
+  // Add an element to the list
+  public void add(T element) {
+    list.add(element);
+  }
+
+  // Get the "first in" element
+  public T getFirstIn() {
+    synchronized (list) {
+      return list.isEmpty() ? null : list.get(0);
+    }
+  }
+
+  // Get the smallest element
+  public T getSmallest() {
+    synchronized (list) {
+      if (list.isEmpty()) {
+        return null;
+      }
+      T smallest = list.get(0);
+      for (T element : list) {
+        if (element.compareTo(smallest) < 0) {
+          smallest = element;
+        }
+      }
+      return smallest;
+    }
+  }
+
+  // Remove the "first in" element
+  public T removeFirstIn() {
+    synchronized (list) {
+      return list.isEmpty() ? null : list.remove(0);
+    }
+  }
+
+  // Remove the smallest element
+  public T removeSmallest() {
+    synchronized (list) {
+      if (list.isEmpty()) {
+        return null;
+      }
+      T smallest = getSmallest();
+      list.remove(smallest);
+      return smallest;
+    }
+  }
+
+  // Check if the queue is empty
+  public boolean isEmpty() {
+    synchronized (list) {
+      return list.isEmpty();
+    }
+  }
+}
+
+class FCAICalc {
+
+  final double v1;
+  final double v2;
+
+  FCAICalc(int lastArrivalTime, int maxBurstTime) {
+    this.v1 = (double) lastArrivalTime / 10;
+    this.v2 = (double) maxBurstTime / 10;
+  }
+
+  int calcFactor(FCAIProcess p) {
+    double factor =
+      (10 - p.getPriority()) +
+      Math.ceil((p.getArrivalTime() / v1)) +
+      Math.ceil((p.getBurstTime() / v2));
+
+    return ((int) Math.ceil(factor));
+  }
+
+  public static void updateQuantum(FCAIProcess p) {
+    if (p.getRemainingQuantum() == 0) {
+      p.setQuantum(p.getQuantum() + 2);
+    } else {
+      p.setQuantum(p.getQuantum() + p.getRemainingQuantum());
+    }
+  }
+}
+
+class FCAIProcess implements Comparable<FCAIProcess>, Runnable {
+
+  protected final String name;
+  protected int executedTime = 0;
+  protected int burstTime;
+  protected final int arrivalTime;
+  protected final int priority;
+  protected int quantum;
+  protected FCAISchedular schedular;
+  protected Boolean running = false;
+  private int remainingQuantum;
+  private FCAICalc calc;
+
+  private static enum Status {
+    NotArrived,
+    Arrived,
+  }
+
+  private Status status = Status.NotArrived;
+
+  public FCAIProcess(
+    String name,
+    int burstTime,
+    int arrivalTime,
+    int priority,
+    int quantum,
+    FCAISchedular schedular
+  ) {
+    this.name = name;
+    this.burstTime = burstTime;
+    this.arrivalTime = arrivalTime;
+    this.priority = priority;
+    this.quantum = quantum;
+    this.schedular = schedular;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getBurstTime() {
+    return burstTime;
+  }
+
+  public int getArrivalTime() {
+    return arrivalTime;
+  }
+
+  public int getPriority() {
+    return priority;
+  }
+
+  public int getQuantum() {
+    return quantum;
+  }
+
+  public void setQuantum(int quantum) {
+    this.quantum = quantum;
+  }
+
+  public int getRemainingQuantum() {
+    return remainingQuantum;
+  }
+
+  public void setCalc(FCAICalc calc) {
+    this.calc = calc;
+  }
+
+  public int getFactor() {
+    return calc.calcFactor(this);
+  }
+
+  @Override
+  public void run() {
+    if (status == Status.NotArrived) {
+      status = Status.Arrived;
+      try {
+        Thread.sleep(arrivalTime * 1000L);
+        // System.out.println("Process " + name + " arrived");
+        new Thread(() -> {
+          schedular.process(this);
+        })
+          .start();
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    } else {
+      // System.out.println("FCAIProcess " + name + " executes");
+      System.out.println(
+        "FCAIProcess " +
+        name +
+        " running -> remainingQuantum:" +
+        quantum +
+        " burstTime: " +
+        burstTime
+      );
+
+      try {
+        remainingQuantum = quantum;
+        running = true;
+        while (running && burstTime > 0 && remainingQuantum > 0) {
+          Thread.sleep(1000L);
+
+          if (running) {
+            burstTime--;
+            remainingQuantum--;
+
+            System.out.println(
+              "FCAIProcess " +
+              name +
+              " running -> remainingQuantum:" +
+              remainingQuantum +
+              " burstTime: " +
+              burstTime
+            );
+            // System.out.println(
+            //   "FCAIProcess " +
+            //   name +
+            //   " running -> remainingQuantum:" +
+            //   remainingQuantum +
+            //   " burstTime: " +
+            //   burstTime
+            // );
+
+            if (running && isPreemptive()) {
+              boolean signal = schedular.signal();
+              if (signal) {
+                if (burstTime > 0) {
+                  running = false;
+                  // System.out.println("Process " + name + " preempted");
+                  // System.out.print(
+                  //   "FCAIProcess " + name + " quantum updated from " + quantum
+                  // );
+                  FCAICalc.updateQuantum(this);
+                  // System.out.println(" to " + quantum);
+                  return;
+                } else if (burstTime == 0) {
+                  running = false;
+                  // System.out.println("FCAIProcess " + name + " finished");
+                  return;
+                }
+              }
+            }
+          }
+        }
+
+        boolean signal = schedular.signal();
+        if (signal) {
+          if (burstTime == 0) {
+            running = false;
+            // System.out.println("FCAIProcess " + name + " finished");
+            return;
+          } else {
+            running = false;
+            // System.out.println("FCAIProcess " + name + " preempted");
+            // System.out.print(
+            // "FCAIProcess " + name + " quantum updated from " + quantum
+            // );
+            FCAICalc.updateQuantum(this);
+            // System.out.println(" to " + quantum);
+            return;
+          }
+        } else {
+          if (burstTime > 0) {
+            running = true;
+            // System.out.print(
+            //   "FCAIProcess " + name + " quantum updated from " + quantum
+            // );
+            FCAICalc.updateQuantum(this);
+            // System.out.println(" to " + quantum);
+            //run again
+            run();
+            return;
+          } else {
+            running = false;
+            // System.out.println("FCAIProcess " + name + " finished");
+            return;
+          }
+        }
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
+
+  public Boolean isPreemptive() {
+    double preemptiveFactor = (quantum - remainingQuantum) / (double) quantum;
+    // System.out.println("preemptiveFactor = " + preemptiveFactor);
+    if (preemptiveFactor >= 0.4) {
+      // System.out.println(p.getName() + " is Preemptive");
+      return true;
+    }
+    // System.out.println(p.getName() + " is not Preemptive");
+    return false;
+  }
+
+  @Override
+  public int compareTo(FCAIProcess other) {
+    return Integer.compare(calc.calcFactor(this), calc.calcFactor(other));
+  }
+}
 
 public class FCAISchedular implements Runnable {
 
@@ -12,6 +306,7 @@ public class FCAISchedular implements Runnable {
   FCAIProcess runningProcess;
   private final CustomQueue<FCAIProcess> waitingQueue;
   protected List<FCAIProcess> processes;
+  private int finishedProcesses;
 
   public FCAISchedular() {
     this.waitingQueue = new CustomQueue<>();
@@ -19,6 +314,27 @@ public class FCAISchedular implements Runnable {
 
   @Override
   public void run() {
+    new Thread(() -> {
+      int g = 0;
+      while (finishedProcesses < processes.size()) {
+        try {
+          Thread.sleep(500);
+          System.out.println(
+            "------------------------------------------------------------------------- time " +
+            g +
+            " -> " +
+            (g + 1)
+          );
+          g++;
+          Thread.sleep(500);
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+          break;
+        }
+      }
+    })
+      .start();
+
     for (FCAIProcess p : processes) {
       new Thread(p).start();
     }
@@ -94,9 +410,9 @@ public class FCAISchedular implements Runnable {
         // and execute the next process in the waiting queue
 
         if (runningProcess.getRemainingQuantum() == 0) {
-          // System.out.println(
-          //   "process " + runningProcess.getName() + " finished the quantum"
-          // );
+          System.out.println(
+            "FCAIprocess " + runningProcess.getName() + " finished the quantum"
+          );
           // runningProcess.interrupt();
           waitingQueue.add(runningProcess);
           runningProcess = waitingQueue.removeFirstIn();
@@ -109,9 +425,10 @@ public class FCAISchedular implements Runnable {
         // then remove it from the running process
         // and execute the next process in the waiting queue
         if (runningProcess.getBurstTime() == 0) {
-          // System.out.println(
-          //   "process " + runningProcess.getName() + " finished"
-          // );
+          System.out.println(
+            "FCAIprocess " + runningProcess.getName() + " finished"
+          );
+          finishedProcesses++;
           // runningProcess.interrupt();
           runningProcess = waitingQueue.removeFirstIn();
           new Thread(runningProcess).start();
@@ -136,6 +453,9 @@ public class FCAISchedular implements Runnable {
           //   " " +
           //   currentFactor
           // );
+          System.out.println(
+            "FCAI process " + runningProcess.getName() + " interrupted"
+          );
           waitingQueue.add(runningProcess);
           // runningProcess.interrupt();
           runningProcess = waitingQueue.removeSmallest();
@@ -157,6 +477,10 @@ public class FCAISchedular implements Runnable {
         return false;
       } else if (runningProcess.getBurstTime() == 0) {
         // System.out.println(" there is no process to execute ");
+        System.out.println(
+          "FCAIprocess " + runningProcess.getName() + " finished"
+        );
+        finishedProcesses++;
         runningProcess = null;
         return true;
       } else {
