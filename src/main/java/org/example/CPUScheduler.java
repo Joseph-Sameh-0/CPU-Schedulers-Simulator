b7a85ae51353235a0509ed881df476446c77c12b
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2024-12-09 01:45:26 +0200

    .

--- src/main/java/org/example/CPUScheduler.java
+++ src/main/java/org/example/CPUScheduler.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 import org.example.FCAI.*;
+import org.example.Priority.NonPreemptivePriority;
 import org.example.SJF.NonPreemptiveSJF;
 
 public class CPUScheduler {
@@ -13,30 +14,28 @@ public class CPUScheduler {
     while (!exit) {
       //let the user select the scheduler
       System.out.println("Select a scheduler: ");
-      System.out.println("1. Shortest- Job First (SJF) Scheduling");
-      System.out.println("2. SRTF Scheduling");
-      System.out.println("3. Priority Scheduling");
+      System.out.println("1. Priority Scheduling");
+      System.out.println("2. Shortest- Job First (SJF) Scheduling");
+      System.out.println("3. SRTF Scheduling");
       System.out.println("4. FCAI Scheduling");
       System.out.println("5. Exit");
       int choice = scanner.nextInt();
       // create a new scheduler based on the user's choice
       switch (choice) {
         case 1:
-          NonPreemptiveSJF.main();
+          NonPreemptivePriority.main();
           break;
         case 2:
-          // schedular = new SRTFScheduler(); // create a new SRTF scheduler
+          NonPreemptiveSJF.main();
           break;
         case 3:
-          // schedular = new PriorityScheduler(); // create a new priority scheduler
+          // schedular = new SRTFScheduler(); // create a new SRTF scheduler
           break;
         case 4:
           final FCAISchedular fCAISchedular;
           fCAISchedular = new FCAISchedular(); // create a new FCAI scheduler
           fCAISchedular.setProcesses("src/test_cases/AG.txt");
-          // new Thread(fCAISchedular).start();
           fCAISchedular.main(scanner); // run the FCAI scheduler
-
           break;
         case 5:
           exit = true;
