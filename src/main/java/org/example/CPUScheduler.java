e78d2dbad9f88a394d2c5d0be443800c086e262b
momenmedhatsalem <momenmedhatsalem@gmail.com>
2024-12-09 18:53:30 +0200

    Almost finished SRTF

--- src/main/java/org/example/CPUScheduler.java
+++ src/main/java/org/example/CPUScheduler.java
@@ -1,38 +1,13 @@
 package org.example;
 
-import org.example.FCAI.*;
+import org.example.SRTF.*;
+
 
 public class CPUScheduler {
 
-  static SRTFSchedular schedular;
+    public static void main(String[] args) {
+        SRTFSimulator.main();
 
-  public static void main(String[] args) {
-    new Thread(() -> {
-      int g = 40;
-      while (g-- > 0) {
-        try {
-          Thread.sleep(500);
-          System.out.println(
-            "------------------------------------------------------------------------- time " + (39 - g) + " -> " +(40 - g)
-          );
-          Thread.sleep(500);
-        } catch (InterruptedException e) {
-          Thread.currentThread().interrupt();
-          break;
-        }
-      }
-    })
-      .start();
 
-    schedular = new SRTFSchedular();
-
-    //get processes
-    schedular.setProcesses("src/test_cases/AG.txt");
-    // List<Process> processes = getProcesses();
-
-    // schedular.setProcesses(processes);
-    //start
-
-    schedular.run();
-  }
+    }
 }
---
+++ src/main/java/org/example/SRTF/SRTFSimulator.java
@@ -0,0 +1,50 @@
+
+package org.example.SRTF;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SRTFSimulator {
+
+    private SRTFSchedular schedular;
+    private List<SRTFProcess> processes;
+
+    public SRTFSimulator(int processCount) {
+        processes = new ArrayList<>();
+        schedular = new SRTFSchedular(processCount);
+        
+        new Thread(schedular).start();
+    }
+
+    public void setProcesses(String filename) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                String processName = parts[0];
+                int burstTime = Integer.parseInt(parts[1]);
+                int arrivalTime = Integer.parseInt(parts[2]);
+
+                processes.add(new SRTFProcess(processName, burstTime, arrivalTime, schedular));
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
+    }
+
+    public void run() {
+        for (SRTFProcess p : processes) {
+            new Thread(p).start();
+        }
+    }
+
+    public static void main() {
+        SRTFSimulator Mainscheduler = new SRTFSimulator(4);
+
+        // Read processes from file
+        Mainscheduler.setProcesses("src/test_cases/SRTF.txt");
+
+        Mainscheduler.run();
+
+    }
+}
